d8ccebedb61432f3ca90fb6e8d2611ca0c8e4022
Md.Mohtasim Shariar <onim04@gmail.com>
2023-01-21 00:26:45 +0600

    okok

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,29 +1,29 @@
-import {Prop,Schema,SchemaFactory} from '@nestjs/mongoose'; 
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-export type UserDocument = HydratedDocument<User>; //eita dara ashole type toiro hoi and database a model er kaj kore. 
-@Schema() 
-export class User { 
-    @Prop({required: true})
-    name: string; 
-    @Prop({required: true})
-    email: string; 
-    @Prop({type: [String],maxlength: 3})
-    hobbies: string[]; 
+export type UserDocument = HydratedDocument<User>; //eita dara ashole type toiro hoi and database a model er kaj kore.
+@Schema()
+export class User {
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: true })
+  email: string;
+  @Prop({ type: [String], maxlength: 3 })
+  hobbies: string[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User); //protita datar jonne type toiri kore class type
-// class User { 
-//     name: string; 
-//     email: string; 
+// class User {
+//     name: string;
+//     email: string;
 //     hobbies: string[]
 // }
 
 /**
- * import mongoose from mongoose; 
+ * import mongoose from mongoose;
  * const userSchema = new mongoose.Schema({
  *  name: String;
- * torBaperName: String; 
- * 
+ * torBaperName: String;
+ *
  * })
  * const User = mongoose.Model('User',userSchema);
  */
